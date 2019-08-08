596e31fdc9f84e51be5887274a36f95e23e89694
Taesu Hyeon <qvil@op.gg>
2019-08-08 19:59:01 +0900

    Complete signup, Modify auth
    - Exception handling to exist user

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,6 @@
 import { Container } from "next/app";
 import GlobalStyles from "../styles/GlobalStyles";
-import * as firebase from "firebase/app";
+import firebase from "firebase/app";
 import firebaseConfig from "../config/firebase";
 import "firebase/auth";
 import "firebase/database";
--- pages/auth/EmailPW.jsx
+++ pages/auth/EmailPW.jsx
@@ -2,28 +2,35 @@ import React, { useState } from "react";
 import firebase from "firebase";
 
 const EmailPW = () => {
-  const [state, setState] = useState({ email: "", password: "" });
+  const initialState = { email: "", password: "" };
+  const [state, setState] = useState(initialState);
   const { email, password } = state;
 
   const handleChange = name => e => {
     setState({ ...state, [name]: e.target.value });
   };
 
-  const signup = e => {
+  const signup = async e => {
     e.preventDefault();
-    console.log("TCL: EmailPW -> email, password", email, password);
-    firebase
-      .auth()
-      .createUserWithEmailAndPassword(email, password)
-      .then(res => {
-        console.log("TCL: EmailPW -> res", res);
-      })
-      .catch(error => {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
-      });
+    try {
+      // const {
+      //   additionalUserInfo: { isNewUser }
+      // } = await firebase.auth().createUserWithEmailAndPassword(email, password);
+      const res = await firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password);
+
+      if (res.additionalUserInfo.isNewUser) {
+        alert("회원가입을 환영합니다!");
+      }
+    } catch ({ code, message }) {
+      console.log("TCL: EmailPW -> code", code);
+      alert(message);
+      // if (message === "EMAIL_EXISTS") {
+      //   alert("이미 존재하는 이메일 입니다!");
+      //   setState(initialState);
+      // }
+    }
   };
   return (
     <div>
