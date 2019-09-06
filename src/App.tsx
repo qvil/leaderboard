062dd770c3381bf7258b692d3953958247478e05
현태수 <qvil@op.gg>
2019-09-07 01:12:16 +0900

    MUI Button test

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
 
 interface IProps {
   title: string;
 }
 
 const App: React.FC<IProps> = ({ title }) => {
-  return <div className="App">{title}</div>;
+  return (
+    <div className="App">
+      {title}
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
+    </div>
+  );
 };
 
 export default App;
